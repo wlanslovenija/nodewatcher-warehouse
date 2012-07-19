484a9651a0a6af8e10795b9b768efbbb771cde5a
Matjaz <matjazd@wlan-si.net>
2012-07-19 07:59:41 +0200

    Added new files.

---
+++ nodewatcherwarehouse/warehouse/forms.py
@@ -0,0 +1,11 @@
+from models import Item, Instance
+from django import forms
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        exclude = ('date_added', 'status_change', 'last_change')
+
+class InstanceForm(forms.ModelForm):
+    class Meta:
+        model = Instance
