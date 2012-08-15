10bb85c0c9e19fec5ac15c839a456d41cf983ae9
Matjaz <matjazd@wlan-si.net>
2012-08-15 13:20:43 +0200

    Models split into Item and Instance, correct foreign key to Node.

--- nodewatcherwarehouse/warehouse/models.py
+++ nodewatcherwarehouse/warehouse/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from macaddress import fields
 
-from nodewatcher.core.models.config import GeneralConfig
+from nodewatcher.nodes.models import Node
 from django.contrib.auth.models import User
 
 class Category(models.Model):
@@ -27,6 +27,16 @@ class Attribute(models.Model):
         return "%s=%s" % (self.name, str(self.value))
 
 class Item(models.Model):
+    name = models.CharField(max_length=50, help_text='Name of the item (e.g. router Linksys WRT54GL, RJ45 crimping tools)')
+    note = models.CharField(max_length=100, blank=True)
+    category = models.ForeignKey(Category)
+    date_added = models.DateField(blank=True)
+    atts = models.ManyToManyField(Attribute, blank=True, null=True)
+    
+    def __str__(self):
+        return u"%s %s" % (str(self.id), self.name)
+
+class Instance(models.Model):
     # property status
     STATUS_CHOICES = (
             ('sold', 'Sold'),
@@ -38,37 +48,24 @@ class Item(models.Model):
             ('wlan-si', 'Used for wlan-si infrastructure'),
             ('warehouse', 'In the warehouse')
     )
-    name = models.CharField(max_length=50)
-    note = models.CharField(max_length=100, blank=True)
-    status = models.CharField(max_length=50, choices = STATUS_CHOICES)
-    amount = models.IntegerField(blank=True, null=True)
-    category = models.ForeignKey(Category)
-    location = models.ForeignKey(Location, blank=True)
-    date_added = models.DateField(blank=True)
-    status_change = models.DateField(blank=True)
-    last_change = models.DateField(blank=True)
-    atts = models.ManyToManyField(Attribute)
     
-    # ref to people
-    member = models.ForeignKey(User, blank=True, null=True)
-    # ref to hot spot location
-    node = models.ForeignKey(GeneralConfig, blank=True, null=True)
-    
-    def __str__(self):
-        return u"%s %s" % (str(self.id), self.name)
-
-class Instance(models.Model):
     name = models.CharField(max_length=50)
     note = models.CharField(max_length=100, blank=True)
     item = models.ForeignKey(Item)
-    status = models.CharField(max_length=50, choices = Item.STATUS_CHOICES)
+    
+    status = models.CharField(max_length=50, choices = STATUS_CHOICES)
     mac = fields.MACAddressField(blank=True, null=True)
     date_added = models.DateField(blank=True, null=True)
-    status_change = models.DateField(blank=True, null=True)
     last_change = models.DateField(blank=True, null=True)
+    
+    amount = models.IntegerField(blank=True, null=True)
+    
+    # location
     location = models.ForeignKey(Location)
+    # ref to people
+    member = models.ForeignKey(User, blank=True, null=True)
+    # ref to hot spot location
+    node = models.ForeignKey(Node, blank=True, null=True)
     
     def __str__(self):
         return "%s %s" % (self.name, str(self.mac))
-    # ref to people
-    # ref to hot spot location
