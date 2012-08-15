7cc7b6d0b04fc07c25b75f5e0a5a3f49100b8d99
Matjaz <matjazd@wlan-si.net>
2012-08-15 13:19:40 +0200

    Renamed items urls.

--- nodewatcherwarehouse/warehouse/urls.py
+++ nodewatcherwarehouse/warehouse/urls.py
@@ -7,11 +7,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', ListView.as_view(model = Item, template_name='item_list.html'), name='item-list'),
-    url(r'^item/add$', ItemAdd.as_view(), name='item-add'),
-    url(r'^instance/list$', ListView.as_view(model = Instance, template_name='instance_list.html'), name='instance-list'),
+    url(r'^items$', ItemAdd.as_view(), name='items'),
+    url(r'^instance/list$', ListView.as_view(model = Instance, template_name='instance_list.html'), name='instances'),
     url(r'^instance/view/(?P<pk>\d+)$', DetailView.as_view(model = Instance, template_name='instance_detail.html'), name='instance-detail'),
-    url(r'^items/list$', ListView.as_view(model = Item, template_name='item_list.html'), name='item-list'),
     url(r'^view/(\d+)$', 'warehouse.views.view'),
     url(r'^admin/', include(admin.site.urls)),
 )
