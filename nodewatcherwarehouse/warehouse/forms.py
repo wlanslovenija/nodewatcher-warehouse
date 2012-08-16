7a76530bc16aa6a4c9b0c5ae60153d11cb797458
Matjaz <matjazd@wlan-si.net>
2012-08-16 19:39:44 +0200

    Node fields are now displayed with node name not id.

--- nodewatcherwarehouse/warehouse/forms.py
+++ nodewatcherwarehouse/warehouse/forms.py
@@ -1,12 +1,29 @@
 from models import Item, Instance
 from django import forms
 
+from nodewatcher.nodes.models import Node
+
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         exclude = ('date_added', 'status_change', 'last_change')
+        widgets = {
+            'note': forms.Textarea(attrs={'cols': 60, 'rows': 5}),
+        }
 
 class InstanceForm(forms.ModelForm):
+    
+    def label_from_instance(self, obj):
+            return obj.name
+    
+    def __init__(self, *args, **kwargs):
+        super(forms.ModelForm, self).__init__(*args, **kwargs)
+        self.fields["node"].label_from_instance = self.label_from_instance
+        self.fields["node"].queryset = Node.objects.regpoint("config").registry_fields(name = "GeneralConfig.name")
+    
     class Meta:
         model = Instance
         exclude = ('date_added', 'status_change', 'last_change')
+        widgets = {
+            'note': forms.Textarea(attrs={'cols': 60, 'rows': 5}),
+        }
