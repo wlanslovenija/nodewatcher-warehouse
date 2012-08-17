8f8c478e05135b57b7c7ebcdfb4a6a4e8eca2c39
Matjaz <matjazd@wlan-si.net>
2012-08-17 08:08:50 +0200

    Added authentication (loged users - see their items, staff - management).

--- nodewatcherwarehouse/warehouse/urls.py
+++ nodewatcherwarehouse/warehouse/urls.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required, user_passes_test
+
 from django.conf.urls import patterns, include, url
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import DetailView
@@ -8,17 +10,20 @@ from warehouse.views import *
 from django.contrib import admin
 admin.autodiscover()
 
+def staff_required(view):
+    return user_passes_test(lambda a: a.is_staff)(view)
+
 urlpatterns = patterns('',
-    url(r'^itemtype$', ItemTypeView.as_view(), name='itemtype'),
-    url(r'^itemtype/(?P<pk>\d+)/delete/$', DeleteViewCustom.as_view(model=ItemType, success_url=reverse_lazy("wh:itemtype") ), name='itemtype-delete'),
+    url(r'^itemtype$', staff_required(ItemTypeView.as_view()), name='itemtype'),
+    url(r'^itemtype/(?P<pk>\d+)/delete/$', staff_required(DeleteViewCustom.as_view(model=ItemType, success_url=reverse_lazy("wh:itemtype"))), name='itemtype-delete'),
     
-    url(r'^item$', ItemView.as_view(), name='item'),
-    url(r'^$', ItemView.as_view(), name='itemview'),
-    url(r'^item/(?P<pk>\d+)/delete/$', DeleteViewCustom.as_view(model=Item, success_url=reverse_lazy("wh:item") ), name='item-delete'),
-    url(r'^item/(?P<pk>\d+)/edit/$', DeleteViewCustom.as_view(model=Item, success_url=reverse_lazy("wh:item") ), name='item-edit'),
-    url(r'^item/(?P<pk>\d+)/view$', DetailView.as_view(model = Item, template_name='item_detail.html'), name='item-detail'),
+    url(r'^item$', staff_required(ItemView.as_view()), name='item'),
+    url(r'^item/(?P<pk>\d+)/delete/$', staff_required(DeleteViewCustom.as_view(model=Item, success_url=reverse_lazy("wh:item"))), name='item-delete'),
+    url(r'^item/(?P<pk>\d+)/edit/$', staff_required(DeleteViewCustom.as_view(model=Item, success_url=reverse_lazy("wh:item"))), name='item-edit'),
+    url(r'^item/(?P<pk>\d+)/view$', staff_required(DetailView.as_view(model = Item, template_name='item_detail.html')), name='item-detail'),
     
-    url(r'^mystuff$', MyStuff.as_view(), name='mystuff'),
+    url(r'^mystuff$', login_required(MyStuff.as_view()), name='mystuff'),
+    url(r'^$', login_required(MyStuff.as_view()), name='mystuff'),
     
-    url(r'^qr_api/(?P<size>\d{1,2})/(?P<string>.*)$', generate_qr, name='qr'),
+    url(r'^qr_api/(?P<size>\d{1,2})/(?P<string>.*)$', login_required(generate_qr), name='qr'),
 )
