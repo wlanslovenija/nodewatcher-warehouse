7de441e37caf38fef3bbffee9aae4122e1b9e72c
Matjaz <matjazd@wlan-si.net>
2012-08-19 00:35:42 +0200

    Added tag for absolute url for warehouse item.

--- nodewatcherwarehouse/warehouse/templatetags/ware_tags.py
+++ nodewatcherwarehouse/warehouse/templatetags/ware_tags.py
@@ -3,6 +3,12 @@ register = template.Library()
 from django.core.urlresolvers import reverse
 
 def qr_code(obj, size=2):
-    return reverse("wh:qr", kwargs={"string": reverse("wh:item-detail", kwargs = {"pk" : obj.id}), "size":size})
+    return reverse("wh:qr", kwargs={"string": abs_view(obj), "size":size})
+
+def abs_view(obj):
+    from django.contrib.sites.models import Site
+    # TODO Somehow check tor https?
+    return "%s://%s%s" % ("http", Site.objects.get_current().domain, reverse("wh:item-detail", kwargs = {"pk" : obj.id}))
 
 register.filter("qr_code", qr_code)
+register.filter("abs_view", abs_view)
