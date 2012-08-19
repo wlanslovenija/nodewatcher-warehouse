d24f523ddb68a51e4e7f3c6b86db2fcf5ea76697
Matjaz <matjazd@wlan-si.net>
2012-08-19 16:13:18 +0200

    Support for multiple item selection (useful for label generation).

--- nodewatcherwarehouse/warehouse/static/warehouse.js
+++ nodewatcherwarehouse/warehouse/static/warehouse.js
@@ -8,7 +8,22 @@ function set_drop_down(id, divs){
     });
 }
 
+function show_message(msg){
+    $("#messagebox").html(msg);
+    $("#messagebox").dialog({
+        resizable: false,
+        height:150,
+        modal: true,
+        buttons: {
+            Ok: function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+}
+
 function show_delete_warning(delete_url){
+    $("#dialog:ui-dialog").dialog("destroy");
     $( "#delete-confirm" ).dialog({
             resizable: false,
             height:150,
@@ -24,4 +39,17 @@ function show_delete_warning(delete_url){
                 }
             }
         });
+}
+
+function generate_labels(){
+    r = new Array();
+    if($("#items_table :checked").length == 0){
+        show_message("Please first select the items from the warehouse!");
+        return;
+    }
+    $("#items_table :checked").each(function(i, el){
+        r.push(el.value)
+    });
+    window.open(labels_url.replace("PLACEHOLDER", r.toString()));
+    $("#items_table :checked").attr("checked", false);
 }
