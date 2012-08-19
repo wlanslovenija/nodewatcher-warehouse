39632d0c3f043ca29bddec8567ce96f248a1c7f6
Matjaz <matjazd@wlan-si.net>
2012-08-19 16:12:42 +0200

    Typo.

--- nodewatcherwarehouse/warehouse/sql/location.sql
+++ nodewatcherwarehouse/warehouse/sql/location.sql
@@ -1,4 +1,4 @@
 INSERT INTO "warehouse_location" VALUES(1,'Skladisce Maribor');
 INSERT INTO "warehouse_location" VALUES(2,'Warehouse at Jadranska');
-INSERT INTO "warehouse_location" VALUES(3,'Crt c drawer at FRI');
+INSERT INTO "warehouse_location" VALUES(3,'Crts drawer at FRI');
 
