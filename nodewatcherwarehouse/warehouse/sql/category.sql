94f5ebe093411f39aa5114fd3aa12ff6fe7d3554
Matjaz <matjazd@wlan-si.net>
2012-08-19 18:23:27 +0200

    Added new type.

--- nodewatcherwarehouse/warehouse/sql/category.sql
+++ nodewatcherwarehouse/warehouse/sql/category.sql
@@ -3,3 +3,4 @@ INSERT INTO "warehouse_category" VALUES(2,'Antenna');
 INSERT INTO "warehouse_category" VALUES(3,'Promotional materials');
 INSERT INTO "warehouse_category" VALUES(4,'Tools');
 INSERT INTO "warehouse_category" VALUES(5,'Development board');
+INSERT INTO "warehouse_category" VALUES(6,'Cable');
