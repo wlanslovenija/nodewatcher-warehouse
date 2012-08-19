9a38cef0d3c721c02673c4e838c3be4ba6d0ae6d
Matjaz <matjazd@wlan-si.net>
2012-08-19 10:27:59 +0200

    Typo.

--- nodewatcherwarehouse/warehouse/templatetags/ware_tags.py
+++ nodewatcherwarehouse/warehouse/templatetags/ware_tags.py
@@ -7,7 +7,7 @@ def qr_code(obj, size=2):
 
 def abs_view(obj):
     from django.contrib.sites.models import Site
-    # TODO Somehow check tor https?
+    # TODO Somehow check for https?
     return "%s://%s%s" % ("http", Site.objects.get_current().domain, reverse("wh:item-detail", kwargs = {"pk" : obj.id}))
 
 register.filter("qr_code", qr_code)
